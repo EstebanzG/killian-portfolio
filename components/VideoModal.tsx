8390f5c7e2601b69f421258a0ac9ec50ca18559c
estebangomez <gomez.esteban@outlook.fr>
2025-07-13 23:03:10 +0200

    Fix responsive

--- components/VideoModal.tsx
+++ components/VideoModal.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function VideoModal({video, onCloseAction}: Props) {
   return (
     <Modal onClose={onCloseAction}>
-        <div className={"bg-[#1B1C1F] p-5 rounded-lg w-1/2 flex flex-col items-center gap-5 overflow-scroll"}>
+        <div className={"bg-[#1B1C1F] p-5 rounded-lg md:w-1/2 flex flex-col items-center gap-5 overflow-scroll"}>
           <h2 className="text-lg text-white font-bold">{video.title}</h2>
           <p className="text-lg text-white font-light">{video.description}</p>
           <div className="aspect-video w-full">
